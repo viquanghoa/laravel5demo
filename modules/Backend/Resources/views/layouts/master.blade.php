d538918ee53cee66a7cd241685303e0c923d91bc
Vi Quang Hoa <vyquanghoa@gmail.com>
2015-09-10 23:14:12 +0700

    a

--- modules/Backend/Resources/views/layouts/master.blade.php
+++ modules/Backend/Resources/views/layouts/master.blade.php
@@ -1,12 +1,39 @@
 <!DOCTYPE html>
-<html lang="en">
-	<head>
-		<meta charset="utf-8">
-		<meta http-equiv="X-UA-Compatible" content="IE=edge">
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<title>Module Backend</title>
-	</head>
-	<body>
-		@yield('content')
-	</body>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Administrator | @yield('title', 'Dashboard')</title>
+    <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
+    @include('admin::partials.style')
+    @yield('style')
+</head>
+<body class="skin-blue fixed">
+
+    @include('admin::partials.header')
+
+    <div class="wrapper row-offcanvas row-offcanvas-left">
+
+        @include('admin::partials.sidebar')
+
+        <!-- Right side column. Contains the navbar and content of the page -->
+        <aside class="right-side">
+            <!-- Content Header (Page header) -->
+            <section class="content-header">
+                @yield('content-header')
+            </section>
+
+            <!-- Main content -->
+            <section class="content">
+                @include('admin::partials.flashes')
+                @yield('content')
+            </section>
+        </aside>
+        <!-- /.right-side -->
+    </div>
+    <!-- ./wrapper -->
+
+    <!-- add new calendar event modal -->
+    @include('admin::partials.script')
+    @yield('script')
+</body>
 </html>
